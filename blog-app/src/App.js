3874c85d877ca405388a32f792c430c8ca382aaf
Tejasvee Dighe <tejasveedighe@gmail.com>
2022-03-22 07:23:32 +0530

    header is the main component nesting all other routes

--- blog-app/src/App.js
+++ blog-app/src/App.js
@@ -1,24 +1,25 @@
-import { BlogList } from "./components/BlogList/BlogList";
 import Header from "./components/Header/Header";
-import TrendingBlogs from "./components/TrendingBlogs/TrendingBlogs";
-import Aside from "./components/Aside/Aside";
-import Body from "./components/Body/Body";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import About from "./pages/About";
+
+const NotFound = () => {
+    return (
+        <div className="not-found">
+            The page you are looking for does not exist please check the URL.
+        </div>
+    );
+};
+
 function App() {
     return (
         <div>
             <BrowserRouter>
                 <Routes>
-                    <Route path="/about" element={<About />} />
-                    {
-                        //<Route path="/write" element={<Write />} />
-                        //<Route path="/signin" element={<SingIn />} />
-                    }{" "}
+                    <Route path="/" element={<Header />}>
+                        <Route path="/about" element={<About />} />
+                        <Route path="*" element={<NotFound />} />
+                    </Route>
                 </Routes>
-                <Header />
-                <TrendingBlogs />
-                <Body BlogList={BlogList} Aside={Aside} />
             </BrowserRouter>
         </div>
     );
--- blog-app/src/components/Header/Header.js
+++ blog-app/src/components/Header/Header.js
@@ -1,35 +1,58 @@
 import "./Header.css";
 import profile from "./images/profile.png";
-import { Routes, Route, Link } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
+
+const Left = () => {
+    return <div className="left">BlogHUB</div>;
+};
+
+const Center = () => {
+    return (
+        <div className="center">
+            <ul className="centerList">
+                <Link className="centerListItem" to="/">
+                    Home
+                </Link>
+                <Link className="centerListItem" to="/about">
+                    About
+                </Link>
+                <Link className="centerListItem" to="/write">
+                    Write
+                </Link>
+                <Link className="centerListItem" to="/signin">
+                    Sign In
+                </Link>
+            </ul>
+        </div>
+    );
+};
+
+const Right = () => {
+    return (
+        <div className="right">
+            <img className="profile" src={profile} alt="profile" />
+            <img
+                className="searchIcon"
+                src="https://img.icons8.com/ios-glyphs/30/000000/search--v1.png"
+                alt="search"
+            />
+        </div>
+    );
+};
+
+const Nav = () => {
+    return (
+        <div>
+            <nav>
+                <Left />
+                <Center />
+                <Right />
+            </nav>
+            <Outlet />
+        </div>
+    );
+};
 
 export default function Header() {
-    return (
-        <nav>
-            <div className="left">BlogHUB</div>
-            <div className="center">
-                <ul className="centerList">
-                    <Link className="centerListItem" to="/">
-                        Home
-                    </Link>
-                    <Link className="centerListItem" to="/about">
-                        About
-                    </Link>
-                    <Link className="centerListItem" to="/write">
-                        Write
-                    </Link>
-                    <Link className="centerListItem" to="/signin">
-                        Sign In
-                    </Link>
-                </ul>
-            </div>
-            <div className="right">
-                <img className="profile" src={profile} alt="profile" />
-                <img
-                    className="searchIcon"
-                    src="https://img.icons8.com/ios-glyphs/30/000000/search--v1.png"
-                    alt="search"
-                />
-            </div>
-        </nav>
-    );
+    return <Nav />;
 }
