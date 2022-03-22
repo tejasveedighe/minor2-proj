accebe8283f2c1081da13460c3bf160690d5443c
Tejasvee Dighe <tejasveedighe@gmail.com>
2022-03-22 07:04:04 +0530

    the front end is messed up refactoring to use router

--- blog-app/src/App.js
+++ blog-app/src/App.js
@@ -3,13 +3,23 @@ import Header from "./components/Header/Header";
 import TrendingBlogs from "./components/TrendingBlogs/TrendingBlogs";
 import Aside from "./components/Aside/Aside";
 import Body from "./components/Body/Body";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import About from "./pages/About";
 function App() {
     return (
         <div>
-            <Header />
-            <TrendingBlogs />
-            <Body BlogList={BlogList} Aside={Aside} />
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/about" element={<About />} />
+                    {
+                        //<Route path="/write" element={<Write />} />
+                        //<Route path="/signin" element={<SingIn />} />
+                    }{" "}
+                </Routes>
+                <Header />
+                <TrendingBlogs />
+                <Body BlogList={BlogList} Aside={Aside} />
+            </BrowserRouter>
         </div>
     );
 }
--- blog-app/src/components/Header/Header.js
+++ blog-app/src/components/Header/Header.js
@@ -1,26 +1,35 @@
 import "./Header.css";
 import profile from "./images/profile.png";
+import { Routes, Route, Link } from "react-router-dom";
 
 export default function Header() {
-	return (
-		<nav>
-			<div className="left">BlogHUB</div>
-			<div className="center">
-				<ul className="centerList">
-					<li className="centerListItem">Home</li>
-					<li className="centerListItem">About</li>
-					<li className="centerListItem">Write</li>
-					<li className="centerListItem">Sign In</li>
-				</ul>
-			</div>
-			<div className="right">
-				<img className="profile" src={profile} alt="profile" />
-				<img
-					className="searchIcon"
-					src="https://img.icons8.com/ios-glyphs/30/000000/search--v1.png"
-					alt="search"
-				/>
-			</div>
-		</nav>
-	);
+    return (
+        <nav>
+            <div className="left">BlogHUB</div>
+            <div className="center">
+                <ul className="centerList">
+                    <Link className="centerListItem" to="/">
+                        Home
+                    </Link>
+                    <Link className="centerListItem" to="/about">
+                        About
+                    </Link>
+                    <Link className="centerListItem" to="/write">
+                        Write
+                    </Link>
+                    <Link className="centerListItem" to="/signin">
+                        Sign In
+                    </Link>
+                </ul>
+            </div>
+            <div className="right">
+                <img className="profile" src={profile} alt="profile" />
+                <img
+                    className="searchIcon"
+                    src="https://img.icons8.com/ios-glyphs/30/000000/search--v1.png"
+                    alt="search"
+                />
+            </div>
+        </nav>
+    );
 }
